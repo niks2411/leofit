5a6b0f6162d0fed646c238a7f3be84d0f6d70f43
faizan-docx <faizanahmad2020.fa@gmail.com>
2025-07-29 02:27:35 +0530

    cursor icon removed

--- src/App.jsx
+++ src/App.jsx
@@ -161,24 +161,7 @@ function App() {
                   </div>
 
                   {/* Scroll Indicator */}
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 1.2 }}
-                    className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-                  >
-                    <motion.div
-                      animate={{ y: [0, 10, 0] }}
-                      transition={{ duration: 2, repeat: Infinity }}
-                      className="w-6 h-10 border-2 border-gray-400 dark:border-gray-500 rounded-full flex justify-center"
-                    >
-                      <motion.div
-                        animate={{ y: [0, 12, 0] }}
-                        transition={{ duration: 2, repeat: Infinity }}
-                        className="w-1 h-3 bg-gray-500 dark:bg-gray-400 rounded-full mt-2"
-                      />
-                    </motion.div>
-                  </motion.div>
+                  
                 </section>
 
                 {/* Stats Section */}
